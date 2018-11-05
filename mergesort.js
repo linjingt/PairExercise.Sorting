a69e9c0fbbf30836827201781db7e60ca02b7a64
Jingtin Tim Lin <lin.jingt@gmail.com>
2018-11-05 14:56:32 -0600

    added merge func

--- mergesort.js
+++ mergesort.js
@@ -9,3 +9,29 @@ function split(wholeArr){
 
   return [firstHalf, secondHalf]
 }
+
+
+function merge([arr1, arr2]){
+  let mergedArr = [];
+  // for (let i = 0; i < arr2.length; i++){
+  //   if (arr1[0] < arr2[i]){
+  //     mergedArr.push(arr1.shift())
+  //   } else {
+  //     mergedArr.push(arr2[i])
+  //   }
+  // }
+
+  while (arr1.length > 0 && arr2.length > 0){
+    if (arr1[0] <= arr2[0]){
+      mergedArr.push(arr1.shift())
+    } else {
+      mergedArr.push(arr2.shift())
+    }
+  }
+
+  mergedArr = mergedArr.concat(arr1);
+  mergedArr = mergedArr.concat(arr2);
+  return mergedArr
+}
+
+
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -3,6 +3,11 @@ describe('Split Array function', function() {
     // your code here
     expect(split([3,1,2])).toEqual([[3,1], [2]])
   });
+});
 
-
+describe('Merge function', function(){
+  it('is able to merge two sorted arrays into one sorted array', function(){
+    // test the merging algorithm
+    expect(merge([[1, 2], [3, 4]])).toEqual([1, 2, 3, 4])
+  });
 });
